7bc87f22ff2f6675b0f5e9d7bdcf137bfe42666d
Joash Mathew <joashm14@gmail.com>
2023-05-14 19:18:04 -0400

    feat: enable umami analytics

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -22,7 +22,7 @@ const siteMetadata = {
     // supports plausible, simpleAnalytics, umami or googleAnalytics
     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
-    umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    umamiWebsiteId: '286653de-d029-4bd9-90ce-150af2a28952', // e.g. 123e4567-e89b-12d3-a456-426614174000
     googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
     posthogAnalyticsId: '', // posthog.init e.g. phc_5yXvArzvRdqtZIsHkEm3Fkkhm3d0bEYUXCaFISzqPSQ
   },
