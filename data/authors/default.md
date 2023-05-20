0da722d2e77d20081fb8b8e6323af0bb300f4f8b
Joash Mathew <joashm14@gmail.com>
2023-05-20 16:19:49 -0400

    chore: fix typo on aboutme + image fixup

--- data/authors/default.md
+++ data/authors/default.md
@@ -11,4 +11,4 @@ github: https://github.com/j0ashm
 
 Joash Mathew is an undergraduate Computer Science student at [Queen's University](https://cs.queensu.ca) in Canada. His interests lie in computer security, and backend development.
 
-Currently, he in an intern at [Distributive](https://distributive.network) working on building a powerful distributed computing platform and ecosystem using web technologies. In his free time, he can be found binging a Netflix show, or playing a video game.
+Currently, he is an intern at [Distributive](https://distributive.network) working on building a powerful distributed computing platform and ecosystem using web technologies. In his free time, he can be found binging a Netflix show, or playing a video game.
--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -16,13 +16,9 @@ export default function AuthorLayout({ children, frontMatter }) {
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
           <div className="flex flex-col items-center pt-8">
-            <Image
-              src={avatar}
-              alt="avatar"
-              width="250px"
-              height="190px"
-              className="h-48 w-48 rounded-full"
-            />
+            <div className="w-50 h-48 overflow-hidden rounded-lg">
+              <Image src={avatar} alt="avatar" width="240px" height="192px" className="" />
+            </div>
             <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
